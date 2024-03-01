2938687b8e8d784cdc5f0d2ab7550a1eecfe9f12
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-03-01 20:53:23 +0100

    103

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  myObject.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
