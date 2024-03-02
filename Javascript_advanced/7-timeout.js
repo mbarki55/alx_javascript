cb03ef0d4869f54443ecbdd59da4dab7901f5144
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-03-02 16:42:06 +0100

    task 7

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+console.log('Start of the execution queue');
+
+for (let i = 0; i < 100; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 0);
+}
+
+console.log('End of the loop printing');
+
+setTimeout(() => {
+  console.log('Final code block to be executed');
+});
