4722e9fc7098971c9701a50a0b66bb7e6c9086c4
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-03-02 16:26:07 +0100

    task 2

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function welcomeMessage(fullName) {
+    return function() {
+      alert('Welcome ' + fullName);
+    };
+  }
+  
+  const guillaume = welcomeMessage('Guillaume');
+  const alex = welcomeMessage('Alex');
+  const fred = welcomeMessage('Fred');
+  
+  guillaume();
+  alex();
+  fred();
+  
+  
